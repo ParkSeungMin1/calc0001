83938bfc4dbb51bdff60ce986d068fad3a6b90c1
ParkSeungMin1 <106250439+ParkSeungMin1@users.noreply.github.com>
2023-04-11 13:05:10 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setuptools.setup(
   long_description_content_type="text/markdown",
   classifiers=[
     "Programming Language :: Python :: 3",
-    "Operation System :: OS Independent",
+    "Operating System :: OS Independent",
   ],
 )
