dedbaf1f8ed2cb387b5d862e742499e8a0837265
ParkSeungMin1 <106250439+ParkSeungMin1@users.noreply.github.com>
2023-04-11 12:40:50 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  inclue_package_data=True,
+  name="",
+  version='0.0.1',
+  description='oss-dev my calculator 0001',
+  author='seungminpark',
+  author_email='1828421@donga.ac.kr',
+  url = "https://github.com/",
+  download_url="",
+  install_requires=['pytest'],
+  long_description='oss-dev calculator python module',
+  long_description_content_type="text/markdown",
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operation System :: OS Independent",
+  ],
+)
